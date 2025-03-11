7dbbc73ad0746db69ec1aaa8682892d61dc898a4
Collin Thao <collinthao685@gmail.com>
2025-03-11 13:09:49 -0500

    Added Advanced OpenGL Section

--- OpenGL_Practice/shader_practice/modules/Model_Loading/module_1_Model/module_1_model.cpp
+++ OpenGL_Practice/shader_practice/modules/Model_Loading/module_1_Model/module_1_model.cpp
@@ -1,143 +1,143 @@
-//#define STB_IMAGE_IMPLEMENTATION
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-#include "../../../stb_image.h"
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
-#include <assimp/Importer.hpp>
-#include <assimp/scene.h>
-#include <assimp/postprocess.h>
-#include <iostream>
-#include "../../../shader.h"
-#include "../../../camera.h"
-#include "../../../model.h"
-#include <memory>
-
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void mouse_callback(GLFWwindow* window, double xpos, double ypos);
-void processInput(GLFWwindow* window);
-void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
-
-glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
-glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
-glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
-glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
-float deltaTime = 0.0f;	// Time between current frame and last frame
-float lastFrame = 0.0f; // Time of last frame
-float fov = 45.f;
-std::shared_ptr<Camera> camera = std::shared_ptr<Camera>(new Camera(cameraPos, deltaTime));
-
-int main() {
-
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-
-    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
-    if (window == NULL)
-    {
-        std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-
-
-    glfwMakeContextCurrent(window);
-
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-    glfwSetCursorPosCallback(window, mouse_callback);
-    glfwSetScrollCallback(window, scroll_callback);
-    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
-    }
-    stbi_set_flip_vertically_on_load(true);
-    glEnable(GL_DEPTH_TEST);
-
-    Shader modelShader = Shader("./modules/Model_Loading/module_1_Model/model_shader.vert", "./modules/Model_Loading/module_1_Model/model_shader.frag");
-    std::shared_ptr<Model> backpack = std::shared_ptr<Model>(new Model("../assets/backpack.obj"));
-
-    /*
-     * __   _____ ___ _____ ___ ___ ___ ___
-     * \ \ / / __| _ \_   _|_ _/ __| __/ __|
-     *  \ V /| _||   / | |  | | (__| _|\__ \
-     *   \_/ |___|_|_\ |_| |___\___|___|___/
-     *
-     */
-
-    modelShader.Use();
-
-    while (!glfwWindowShouldClose(window))
-    {
-
-        processInput(window);
-
-        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        float currentFrame = glfwGetTime();
-        deltaTime = currentFrame - lastFrame;
-        lastFrame = currentFrame;
-
-        camera->Update(window, deltaTime);
-
-        glm::mat4 model = glm::mat4(1.0f);
-
-        glm::mat4 view = glm::mat4(1.0f);
-        // note that we're translating the scene in the reverse direction of where we want to move
-
-        glm::mat4 projection;
-        projection = glm::perspective(glm::radians(fov), 800.0f / 600.0f, 0.1f, 100.0f);
-
-        view = glm::lookAt(camera->CameraPos, camera->CameraPos + camera->CameraFront, camera->CameraUp);
-
-        modelShader.Use();
-
-        modelShader.SetMat4("model", model);
-        modelShader.SetMat4("view", view);
-        modelShader.SetMat4("projection", projection);
-
-        backpack->Draw(modelShader);
-
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-
-    }
-
-    glfwTerminate();
-
-    return 0;
-
-}
-
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-    glViewport(0, 0, width, height);
-}
-
-
-void processInput(GLFWwindow* window)
-{
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
-}
-
-void mouse_callback(GLFWwindow* window, double xpos, double ypos)
-{
-    camera->MoveCamera(xpos, ypos);
-}
-
-void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
-{
-    fov -= (float)yoffset;
-    if (fov < 1.0f)
-        fov = 1.0f;
-    if (fov > 45.0f)
-        fov = 45.0f;
-}
+////#define STB_IMAGE_IMPLEMENTATION
+//#include <glad/glad.h>
+//#include <GLFW/glfw3.h>
+//#include "../../../stb_image.h"
+//#include <glm/glm.hpp>
+//#include <glm/gtc/matrix_transform.hpp>
+//#include <glm/gtc/type_ptr.hpp>
+//#include <assimp/Importer.hpp>
+//#include <assimp/scene.h>
+//#include <assimp/postprocess.h>
+//#include <iostream>
+//#include "../../../shader.h"
+//#include "../../../camera.h"
+//#include "../../../model.h"
+//#include <memory>
+//
+//void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+//void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+//void processInput(GLFWwindow* window);
+//void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+//
+//glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+//glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+//glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+//glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+//float deltaTime = 0.0f;	// Time between current frame and last frame
+//float lastFrame = 0.0f; // Time of last frame
+//float fov = 45.f;
+//std::shared_ptr<Camera> camera = std::shared_ptr<Camera>(new Camera(cameraPos, deltaTime));
+//
+//int main() {
+//
+//    glfwInit();
+//    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+//    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+//    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+//    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+//
+//    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+//    if (window == NULL)
+//    {
+//        std::cout << "Failed to create GLFW window" << std::endl;
+//        glfwTerminate();
+//        return -1;
+//    }
+//
+//
+//    glfwMakeContextCurrent(window);
+//
+//    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+//    glfwSetCursorPosCallback(window, mouse_callback);
+//    glfwSetScrollCallback(window, scroll_callback);
+//    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+//
+//    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+//    {
+//        std::cout << "Failed to initialize GLAD" << std::endl;
+//        return -1;
+//    }
+//    stbi_set_flip_vertically_on_load(true);
+//    glEnable(GL_DEPTH_TEST);
+//
+//    Shader modelShader = Shader("./modules/Model_Loading/module_1_Model/model_shader.vert", "./modules/Model_Loading/module_1_Model/model_shader.frag");
+//    std::shared_ptr<Model> backpack = std::shared_ptr<Model>(new Model("../assets/backpack.obj"));
+//
+//    /*
+//     * __   _____ ___ _____ ___ ___ ___ ___
+//     * \ \ / / __| _ \_   _|_ _/ __| __/ __|
+//     *  \ V /| _||   / | |  | | (__| _|\__ \
+//     *   \_/ |___|_|_\ |_| |___\___|___|___/
+//     *
+//     */
+//
+//    modelShader.Use();
+//
+//    while (!glfwWindowShouldClose(window))
+//    {
+//
+//        processInput(window);
+//
+//        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+//        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+//
+//        float currentFrame = glfwGetTime();
+//        deltaTime = currentFrame - lastFrame;
+//        lastFrame = currentFrame;
+//
+//        camera->Update(window, deltaTime);
+//
+//        glm::mat4 model = glm::mat4(1.0f);
+//
+//        glm::mat4 view = glm::mat4(1.0f);
+//        // note that we're translating the scene in the reverse direction of where we want to move
+//
+//        glm::mat4 projection;
+//        projection = glm::perspective(glm::radians(fov), 800.0f / 600.0f, 0.1f, 100.0f);
+//
+//        view = glm::lookAt(camera->CameraPos, camera->CameraPos + camera->CameraFront, camera->CameraUp);
+//
+//        modelShader.Use();
+//
+//        modelShader.SetMat4("model", model);
+//        modelShader.SetMat4("view", view);
+//        modelShader.SetMat4("projection", projection);
+//
+//        backpack->Draw(modelShader);
+//
+//        glfwSwapBuffers(window);
+//        glfwPollEvents();
+//
+//    }
+//
+//    glfwTerminate();
+//
+//    return 0;
+//
+//}
+//
+//void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+//{
+//    glViewport(0, 0, width, height);
+//}
+//
+//
+//void processInput(GLFWwindow* window)
+//{
+//    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+//        glfwSetWindowShouldClose(window, true);
+//}
+//
+//void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+//{
+//    camera->MoveCamera(xpos, ypos);
+//}
+//
+//void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+//{
+//    fov -= (float)yoffset;
+//    if (fov < 1.0f)
+//        fov = 1.0f;
+//    if (fov > 45.0f)
+//        fov = 45.0f;
+//}
